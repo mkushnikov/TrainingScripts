cb0beb44b4c56d8410ec7b07d0b2c5fba46dfdef
mkushnikov <kushnikov.mikhail@gmail.com>
2020-03-11 22:15:53 +0200

    Creation of the repository.
    Created inital structure of the scripts storage.
    Uploaded first script.

---
+++ Assets/Scripts/MyLittleCircle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MyLittleCircle : MonoBehaviour
+{
+    public float MoveSpeed { get; set; }
+
+    public Vector3 GetPosition { get => transform.position; }
+
+    public void MoveToTarget(Vector3 targetPoint)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint, MoveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpeedSlider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedSlider : MonoBehaviour
+{
+    [SerializeField] private GameObject _myCircle;
+    [SerializeField] private Text _currentSpeedLabel;
+
+    private void Awake() => ChangeSpeed();
+    public void ChangeSpeed()
+    {
+        _currentSpeedLabel.text = GetComponent<Slider>().value.ToString();
+        _myCircle.GetComponent<MyLittleCircle>().MoveSpeed = GetComponent<Slider>().value;
+    }
+}
