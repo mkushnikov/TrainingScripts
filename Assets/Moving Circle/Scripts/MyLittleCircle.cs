b722aae5e6a2c30da477785ae502220dc035b0c1
mkushnikov <kushnikov.mikhail@gmail.com>
2020-03-14 18:42:06 +0200

    Project restructuring
    Changed hierarchy of scenes and resources.

---
+++ Assets/Moving Circle/Scripts/MyLittleCircle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace MovingCirle
+{
+    public class MyLittleCircle : MonoBehaviour
+    {
+        public float MoveSpeed { get; set; }
+
+        public Vector3 GetPosition { get => transform.position; }
+
+        public void MoveToTarget(Vector3 targetPoint)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPoint, MoveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Moving Circle/Scripts/SpeedSlider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MovingCirle
+{
+    public class SpeedSlider : MonoBehaviour
+    {
+        [SerializeField] private GameObject _myCircle;
+        [SerializeField] private Text _currentSpeedLabel;
+
+        private void Awake() => ChangeSpeed();
+        public void ChangeSpeed()
+        {
+            _currentSpeedLabel.text = GetComponent<Slider>().value.ToString();
+            _myCircle.GetComponent<MyLittleCircle>().MoveSpeed = GetComponent<Slider>().value;
+        }
+    }
+}
